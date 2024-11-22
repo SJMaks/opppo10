3d8438e3f3a42dd47ecb36e050e56c1e258e94ff
SJMaks <kraev.maksim2003@gmail.com>
2024-11-22 17:02:17 +0700

    fixed negative power

--- main.cpp
+++ main.cpp
@@ -2,16 +2,24 @@
 
 using namespace std;
 
-double my_pow(double num, unsigned int pow)
+double my_pow(double num, int pow)
 {
 	double result = num;
-	for (int i = 1; i < pow; i++) {
-		result *= num;
+	if (pow < 0) {
+		for (int i = 1; i < -pow; i++) {
+			result *= num;
+		}
+		result = 1 / result;
+	}
+	else {
+		for (int i = 1; i < pow; i++) {
+			result *= num;
+		}
 	}
 	return result;
 }
 
 int main()
 {
-	cout << my_pow(2, 5);
+	cout << my_pow(2, -5);
 }
